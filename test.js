fefb7e4929741b9bf46204e279ba55b618e22954
Nazmul Islam <nazmuul420@gmail.com>
2024-06-05 15:50:13 +0600

    test.js is clonner

--- test.js
+++ test.js
@@ -1,47 +1,52 @@
 const { MongoClient } = require("mongodb");
 
-async function main() {
+async function clone(from, to) {
   /**
    * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
    * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
    */
-  const uri =
-    "mongodb+srv://spekterbangla:bPbpqQtXGUh8sQc2@cluster0.ujhky.mongodb.net/cloudbreak-prod?retryWrites=true&w=majority";
-  const uri2 = "mongodb://127.0.0.1:27017/cloudbreak-dev";
-
-  const client = new MongoClient(uri);
-  const client2 = new MongoClient(uri2);
+  const fromClient = new MongoClient(from.uri);
+  const toClient = new MongoClient(to.uri);
 
   try {
     // Connect to the MongoDB cluster
-    await client.connect();
-    await client2.connect();
-    const db = client.db("cloudbreak-prod");
-    const db2 = client2.db("cloudbreak-dev");
+    await fromClient.connect();
+    await toClient.connect();
+    const fromDb = fromClient.db(from.db);
+    const toDb = toClient.db(to.db);
 
-    const tables = await db.collections({ nameOnly: true });
-    for (let i = 0; i < tables.length; i++) {
-      const table = tables[i];
-      const collectionName = table.collectionName;
+    const fromTables = await fromDb.collections({ nameOnly: true });
+    for (let i = 0; i < fromTables.length; i++) {
+      const fromTable = fromTables[i];
+      const collectionName = fromTable.collectionName;
       if (collectionName == "system.indexes") {
         continue;
       }
 
-      const docs = await table.find({}).toArray();
+      const docs = await fromTable.find({}).toArray();
 
       if (docs.length > 0) {
-        const table2 = db2.collection(collectionName);
-        await table2.deleteMany({});
+        const toTable = toDb.collection(collectionName);
+        await toTable.deleteMany({});
         console.log(`inserting into ${collectionName}`);
-        await table2.insertMany(docs);
+        await toTable.insertMany(docs);
       }
     }
   } catch (e) {
     console.error(e);
   } finally {
-    await client.close();
-    await client2.close();
+    await fromClient.close();
+    await toClient.close();
   }
 }
 
-main().catch(console.error);
+clone(
+  {
+    uri: "mongodb+srv://spekterbangla:bPbpqQtXGUh8sQc1@cluster0.ujhky.mongodb.net/?retryWrites=true&w=majority",
+    db: "atlashub-prod-clone",
+  },
+  {
+    uri: "mongodb+srv://dev:12345@testcluster.qhiapda.mongodb.net/?retryWrites=true&w=majority",
+    db: "atlashub-dev",
+  },
+).catch(console.error);
